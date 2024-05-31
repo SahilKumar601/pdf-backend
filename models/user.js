30ac95314bafc58684aedcabd4829617d7ad1de0
Jacob George <134957111+jacobgeorge5566@users.noreply.github.com>
2024-05-31 22:48:33 +0530

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,24 +1,25 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  friendRequests: [{ type: Schema.Types.ObjectId, ref: "user" }],
-  friends: [{ type: Schema.Types.ObjectId, ref: "user" }],
-  summarizedPDFs: [{ type: Schema.Types.ObjectId, ref: "summarizedPDF" }],
+const mongoose = require('mongoose')
+const {Schema}=mongoose;
+const UserSchema =new Schema ({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    friendRequests: [{ type: String , ref: 'email' }],
+    friends: [{ type: String , ref: 'email' }]
 });
-module.exports = mongoose.model("user", UserSchema);
+module.exports =mongoose.model('user',UserSchema);
