61e87c238365f51442a4bacb032c54e359df83ad
AdarshKumar5597 <adarshkumar5597@gmail.com>
2024-06-03 01:15:13 +0530

    made changes in friends routes

--- routes/friend.js
+++ routes/friend.js
@@ -1,138 +1,208 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../modles/user'); 
+const User = require("../models/user");
 
+router.post("/sendRequest", async (req, res) => {
+  const { Uemail, Femail } = req.body;
 
+  try {
+    const user = await User.findOne({ email: Uemail });
+    const friend = await User.findOne({ email: Femail });
 
-
-router.post('/sendRequest', async (req, res) => {
-    const { Uemail, Femail } = req.body;
-
-    try {
-        const user = await User.findOne({ email: Uemail });
-        const friend = await User.findOne({ email: Femail });
-
-        if (!user || !friend) {
-            return res.status(404).json({ msg: 'User not found' });
-        }
-
-        if (friend.friendRequests.includes(Uemail)) {
-            return res.status(400).json({ msg: 'Friend request already sent' });
-        }
-        if (user===friend){
-            return res.status(400).json({ msg: 'You can not send friend request to yourself' });
-
-        }
-        if (friend.friends.includes(Uemail)) {
-            return res.status(400).json({ msg: 'You are already friends' });
-        }
-
-        friend.friendRequests.push(Uemail);
-        await friend.save();
-
-        res.status(200).json({ msg: 'Friend request sent' });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
+    if (!user || !friend) {
+      return res.status(404).json({ success: false, msg: "User not found" });
     }
+
+    if (friend.friendRequests.includes(Uemail)) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Friend request already sent" });
+    }
+    if (user === friend) {
+      return res.status(400).json({
+        success: false,
+        msg: "You can not send friend request to yourself",
+      });
+    }
+    if (friend.friends.includes(Uemail)) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "You are already friends" });
+    }
+
+    friend.friendRequests.push(Uemail);
+    await friend.save();
+
+    res.status(200).json({ success: true, msg: "Friend request sent" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
 });
 
+router.post("/acceptRequest", async (req, res) => {
+  const { Uemail, Femail } = req.body;
 
-router.post('/acceptRequest', async (req, res) => {
-    const { Uemail, Femail } = req.body;
+  try {
+    const user = await User.findOne({ email: Uemail });
+    const friend = await User.findOne({ email: Femail });
 
-    try {
-        const user = await User.findOne({ email: Uemail });
-        const friend = await User.findOne({ email: Femail });
-
-        if (!user || !friend) {
-            return res.status(404).json({ msg: 'User not found' });
-        }
-
-        if (!user.friendRequests.includes(Femail)) {
-            return res.status(400).json({ msg: 'No friend request found' });
-        }
-
-        user.friendRequests = user.friendRequests.filter(request => request.toString() !== Femail);
-        user.friends.push(Femail);
-        await user.save();
-
-        friend.friends.push(Uemail);
-        await friend.save();
-
-        res.status(200).json({ msg: 'Friend request accepted' });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
+    if (!user || !friend) {
+      return res.status(404).json({
+        success: false,
+        msg: "User not found",
+      });
     }
+
+    if (!user.friendRequests.includes(Femail)) {
+      return res.status(400).json({
+        success: false,
+        msg: "No friend request found",
+      });
+    }
+
+    user.friendRequests = user.friendRequests.filter(
+      (request) => request.toString() !== Femail
+    );
+    user.friends.push(Femail);
+    await user.save();
+
+    friend.friends.push(Uemail);
+    await friend.save();
+
+    res.status(200).json({
+      success: true,
+      msg: "Friend request accepted",
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
 });
 
+router.post("/rejectRequest", async (req, res) => {
+  const { Uemail, Femail } = req.body;
 
-router.post('/rejectRequest',async (req, res) => {
-    const { Uemail, Femail } = req.body;
+  try {
+    const user = await User.findOne({ email: Uemail });
 
-    try {
-        const user = await User.findOne({ email: Uemail });
-
-        if (!user) {
-            return res.status(404).json({ msg: 'User not found' });
-        }
-
-        if (!user.friendRequests.includes(Femail)) {
-            return res.status(400).json({ msg: 'Friend request not found' });
-        }
-
-        user.friendRequests = user.friendRequests.filter(id => id.toString() !== Femail);
-        await user.save();
-
-        res.status(200).json({ msg: 'Friend request rejected' });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        msg: "User not found",
+      });
     }
+
+    if (!user.friendRequests.includes(Femail)) {
+      return res.status(400).json({
+        success: false,
+        msg: "Friend request not found",
+      });
+    }
+
+    user.friendRequests = user.friendRequests.filter(
+      (id) => id.toString() !== Femail
+    );
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      msg: "Friend request rejected",
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
 });
 
+router.post("/getFriendRequests", async (req, res) => {
+  const { Uemail } = req.body;
 
+  try {
+    const user = await User.findOne({ email: Uemail });
 
-router.post('/getFriendRequests',async (req, res) => {
-    const { Uemail} = req.body;;
-
-    try {
-        const user = await User.findOne({ email: Uemail });
-
-        if (!user) {
-            return res.status(404).json({ msg: 'User not found' });
-        }
-
-        res.status(200).json(user.friendRequests);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        msg: "User not found",
+      });
     }
+
+    res.status(200).json({
+      success: true,
+      msg: "Friend requests",
+      data: user.friendRequests,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
 });
 
-router.post('/getFriends',async (req, res) => {
-    const { Uemail} = req.body;
+router.post("/getFriends", async (req, res) => {
+  const { Uemail } = req.body;
 
-    try {
-        const user = await User.findOne({ email: Uemail });
+  try {
+    const user = await User.findOne({ email: Uemail });
 
-        if (!user) {
-            return res.status(404).json({ msg: 'User not found' });
-        }
-
-        res.status(200).json(user.friends);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        msg: "User not found",
+      });
     }
+
+    res.status(200).json({
+      success: true,
+      msg: "Friends",
+      data: user.friends,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
 });
 
+router.post("/searchFriend", async (req, res) => {
+  const { Femail } = req.body;
 
+  try {
+    const user = await User.findOne({ Femail });
 
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        msg: "Friend not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      msg: "Friend found",
+      data: user,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      success: false,
+      msg: "Server Error",
+    });
+  }
+});
 
 module.exports = router;
-
-
-
-
